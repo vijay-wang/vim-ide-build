b51b53c9952eb15ed4a020df68e3daea3ea3e168
vijay-wang <ww107587@gmail.com>
2022-11-18 14:01:53 +0800

    modified:   vim-ide-build.sh

--- vim-ide-build.sh
+++ vim-ide-build.sh
@@ -1,10 +1,6 @@
 vundle_url="https://github.com/VundleVim/Vundle.vim.git"
 
-function print_err() {
-	echo "\n-------------------\n$1-------------------\n"
-}
-
-git clone $vundle_url ~/.vim/bundle/Vundle.vim || print_err "clone vundle faild!!!!"
+git clone $vundle_url ~/.vim/bundle/Vundle.vim
 mv ~/.vimrc .vimrc_bck
 cp .vimrc ~/.vimrc
 vim +PluginInstall +qall
@@ -14,4 +10,4 @@ sudo apt install build-essential cmake vim-nox python3-dev
 sudo apt install mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre npm
 cd ~/.vim/bundle/YouCompleteMe
 git submodule update --init --recursive
-python3 install.py --all
+python3 install.py --clang-completer
